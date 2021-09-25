76e77396fc7472b60c6a666253bba0e1b2d96fc4
Jagjiwan Chimkar <59138210+JagjiwanChimkar@users.noreply.github.com>
2021-09-25 11:52:09 +0530

    median brute

---
+++ Matrix/MedianInRowSort/Brute.cpp
@@ -0,0 +1,18 @@
+//https://practice.geeksforgeeks.org/problems/median-in-a-row-wise-sorted-matrix1527/1
+//Time Comp: O(R*C)
+//Space Comp: O(R*C)
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int r, int c){
+        vector<int> arr;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                arr.push_back(matrix[i][j]);
+            }
+        }
+        
+        sort(arr.begin(),arr.end());
+        return arr[(r*c)/2];
+    }
+};
