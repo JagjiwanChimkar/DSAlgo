e0273b10dbfe9a7a68e84e39ff407b49132f72c4
Jagjiwan Chimkar <59138210+JagjiwanChimkar@users.noreply.github.com>
2021-09-26 11:49:20 +0530

    remove error

--- Matrix/MedianInRowSort/BinarySearch.cpp
+++ Matrix/MedianInRowSort/BinarySearch.cpp
@@ -7,34 +7,33 @@
 //Time Comp: O(32*r*log(c))
 //Spac Comp: O(1)
 
+
 class Solution{   
 public:
     int median(vector<vector<int>> &matrix, int r, int c){
-       int min=INT_MIN,max=INT_MAX;
-       for(int i=0;i<r;i++){
-           if(matrix[i][0]<min){
-               min=matrix[i][0];
-           }
-           if(matrix[i][c-1]>max){
-               max=matrix[i][c-1];
-           }
-       }
-       
-       int med_count=(r*c+1)/2;
-       
-       while(min<max){
-           int count=0;
-           int mid=(min+(max-min))/2;
-           for(int i=0;i<r;i++){
-               count+=(upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin());
-           }
-           if(count<med_count){
-               min=mid+1;
-           }else{
-               max=mid;
-           }
-       }
-       
-       return min;
+        int mi=INT_MAX,mx=INT_MIN;
+        for(int i=0;i<r;i++){
+            mi=min(mi,matrix[i][0]);
+        }
+        for(int i=0;i<r;i++){
+            mx=max(mx,matrix[i][c-1]);
+        }
+        
+        int med_count=(r*c+1)/2;
+        
+        while(mi<mx){
+            int mid=(mi+mx)/2;
+            int count=0;
+            for(int i=0;i<r;i++){
+                count+=upper_bound(matrix[i].begin(),matrix[i].end(),mid)-matrix[i].begin();
+            }
+            
+            if(count<med_count){
+                mi=mid+1;
+            }else{
+                mx=mid;
+            }
+        }
+        return mi;
     }
 };
