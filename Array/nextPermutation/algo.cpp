9a0f9f65647276b6a34bacac727198d6a100a844
Jagjiwan Chimkar <59138210+JagjiwanChimkar@users.noreply.github.com>
2021-10-01 13:19:04 +0530

    next permutation

---
+++ Array/nextPermutation/algo.cpp
@@ -0,0 +1,33 @@
+//1. Tranverse from right and check which index1 is arr[i]<arr[i+1] => 1 "3 5" 4 2
+//2. Tranverse from right and check which index 2 is arr[index2]>arr[index1] => 1 '3' 5 "4" 2
+//3. swap(arr[index1],arr[index2]); => 1 '4' 5 '3' 2
+//4. reverse(index1+1,n); => 1 4 "2 3 5"
+
+//Time Comp:O(n)
+//Space Comp: O(1)
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n=nums.size();
+        int ind1=-1,ind2;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                ind1=i;
+                break;
+            }
+        }
+        if(ind1!=-1){
+            for(int i=n-1;i>ind1;i--){
+                if(nums[i]>nums[ind1]){
+                    ind2=i;
+                    break;
+                }
+            }
+            swap(nums[ind1],nums[ind2]);
+        }
+        
+        reverse(nums.begin()+ind1+1,nums.end());
+        
+        
+    }
+};
